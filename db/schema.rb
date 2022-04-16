233b9ecb97f78fbbea7a0a952200e50dbd3802aa
KanaKarimata <kana.lt22@gmail.com>
2022-04-16 08:10:22 +0000

    [Fixed]kadai3 resubmit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_115332) do
+ActiveRecord::Schema.define(version: 2022_04_16_080216) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 2022_04_14_115332) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "profile_images", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
